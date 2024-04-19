3970c31aad66c88d1802f7a07eb638678633c68c
K-kare <kamunawangari@gmail.com>
2024-04-19 11:32:11 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-#create a file
 file { '/tmp/example.txt':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet\n"
+  content => "I love Puppet\n",
 }
