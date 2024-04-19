a2ca22e38d179d14a474c8af58ec7b1b338c42d7
K-kare <kamunawangari@gmail.com>
2024-04-19 11:03:14 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#create a file
+file { '/tmp/example.txt':
+  ensure  => file,
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => "This is an example file created by Puppet\n",
+}
